a0e7cd14a824d759a7f532dc3946f304b97b49bd
Pratyush Rao <pratyur06@gmail.com>
2026-02-07 14:03:07 +0530

    trials

--- backend/main.py
+++ backend/main.py
@@ -106,8 +106,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True,
+    allow_origins=[*], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
